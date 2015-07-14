02a4758a8c1e13fc1c9dfbde6341da76bd3e8a5e
loveemu <loveemu@gmail.com>
2015-07-14 20:29:23 +0900

    Add one-line note to Windows batch script

--- PetiteMM.bat
+++ PetiteMM.bat
@@ -4,6 +4,8 @@
 @for %%a in (%*) do @java -jar "%~dp0PetiteMM.jar" %PMMOPTS% "%%~fa"
 @goto heaven
 :usage
+@echo Note: Drag and drop MIDI files into the batch script
+@echo.
 @java -jar "%~dp0PetiteMM.jar"
 :heaven
 @endlocal
